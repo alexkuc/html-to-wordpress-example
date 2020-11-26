da71ad15ec3ddd1c5cde7161d81ed2817b572c59
Alexander Kucheryuk <3331946+alexkuc@users.noreply.github.com>
2020-11-27 00:16:51 +0200

    Complete sample page

--- page-sample-page.php
+++ page-sample-page.php
@@ -12,7 +12,17 @@
   <?php get_template_part('template-parts/header') ?>
 
   <!-- Main Content -->
-  
+  <article>
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-8 col-md-10 mx-auto">
+          <?php the_content() ?>
+        </div>
+      </div>
+    </div>
+  </article>
+
+  <hr>
 
   <!-- Footer -->
   <?php get_template_part('template-parts/footer') ?>
