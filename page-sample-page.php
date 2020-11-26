a14d502aa2a2b2685cdb5e3328f72c985e422240
Alexander Kucheryuk <3331946+alexkuc@users.noreply.github.com>
2020-11-26 21:38:45 +0200

    Create pages about, contact and sample

---
+++ page-sample-page.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<!-- Head -->
+<?php get_template_part('template-parts/head') ?>
+
+<body>
+  <!-- Navigation -->
+  <?php get_template_part('template-parts/navigation') ?>
+
+  <!-- Page Header -->
+  <?php get_template_part('template-parts/header') ?>
+
+  <!-- Main Content -->
+  
+
+  <!-- Footer -->
+  <?php get_template_part('template-parts/footer') ?>
+
+  <!-- WordPress Footer -->
+  <?php wp_footer() ?>
+</body>
+
+</html>
