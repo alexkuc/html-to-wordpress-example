8605771f4feca555cdc75358e1737a3071c38680
Alexander Kucheryuk <3331946+alexkuc@users.noreply.github.com>
2020-11-27 01:42:46 +0200

    Refactor function dd() using

--- functions.php
+++ functions.php
@@ -8,10 +8,7 @@ require ROOT . '/vendor/autoload.php';
 
 require ROOT . '/php/bootstrap.php';
 
-function dd($value = null)
+function dd()
 {
-  array_map(function ($x) {
-    var_dump($x);
-  }, func_get_args());
-  die;
+  die(var_dump(func_get_args()));
 }
