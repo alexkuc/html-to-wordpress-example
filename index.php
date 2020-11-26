871c94b9072e4c16c77184ef7a4eef8282549c62
Alexander Kucheryuk <3331946+alexkuc@users.noreply.github.com>
2020-11-26 05:36:29 +0200

    Add template part meta tags

--- index.php
+++ index.php
@@ -2,11 +2,7 @@
 <html lang="en">
 
 <head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <meta name="description" content="">
-  <meta name="author" content="">
-
+  <?php get_template_part('template-parts/meta-tags') ?>
   <!-- WordPress Header -->
   <?php wp_head() ?>
 </head>
